7acff6470d40e64d1ad2a89880f8b68afed6240e
Kanak Rajput <rajputkanak078@gmail.com>
2025-03-07 02:14:47 +0530

    [kanak]Add. Added validation function to validate all the fields

--- Main.js
+++ Main.js
@@ -1,4 +1,4 @@
 import {AddressBookContact} from"./AddressBookContact.js";
-let contact1=new AddressBookContact("kanak","Rajput","185A","Bhopal","Madhya pradesh",462021,343444445,"kanak@example.com")
+let contact1=new AddressBookContact("Kanak","Rajput","185A","Bhopal","Madhya pradesh",462021,6263733333,"kanak@example.com")
 console.log(contact1)
 
