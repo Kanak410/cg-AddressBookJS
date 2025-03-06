4f8865b94185262e1614f423717d9d714e01d084
Kanak Rajput <rajputkanak078@gmail.com>
2025-03-07 03:11:46 +0530

    [kanak]Add. Added support of finding and editing details by name

--- Main.js
+++ Main.js
@@ -11,6 +11,15 @@ class AddressBook{
         console.log("Address Book Contact");
         this.contacts.forEach((contact,index) => console.log(`${index+1}.${contact.toString()}`));
     }
+    findAndEditContact(name,newContact){
+        let contact=this.contacts.find(c=>c.first_name==name);
+        if(contact){
+            Object.assign(contact,newContact);
+            console.log(`contact update sucessfully : `+contact);
+        }else{
+            console.log(`contact with name "${name} not found!"`);
+        }
+    }
 }
     const addressBook=new AddressBook();
 
@@ -19,5 +28,7 @@ let contact1=new AddressBookContact("Kanak","Rajput","185A","Bhopal","Madhya pra
 let contact2=new AddressBookContact("Som","Rajput","73 Avenue","Itarsi","Madhya Pradesh",46115,9187536781,"som@example.com")
 addressBook.addContact(contact1)
 addressBook.addContact(contact2)
-addressBook.displayContact()
+addressBook.findAndEditContact("Kanak",{phone_number:123456789,city:"jabalpur"})
+addressBook.findAndEditContact("Som",{phone_number:987654321,city:"Itarsi"})
+//addressBook.displayContact()
 
